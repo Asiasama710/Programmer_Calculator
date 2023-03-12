32b76dd7a9752fb7a2296a6420dc07c8fa17fdd9
Asia sama <sehunexo710@gmail.com>
2023-03-12 05:10:36 +0300

    fix text field validate & improve code

--- app/src/main/java/com/asiasama/programmercalculator/MainActivity.kt
+++ app/src/main/java/com/asiasama/programmercalculator/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var octalResult: EditText
-    private lateinit var hexadecimalResult: EditText
-    private lateinit var binaryResult: EditText
-    private lateinit var decimalResult: EditText
+    private lateinit var octalEditText: EditText
+    private lateinit var hexadecimalEditText: EditText
+    private lateinit var binaryEditText: EditText
+    private lateinit var decimalEditText: EditText
     private var textWatcher: TextWatcher? = null
-    private lateinit var currentFocusedView: EditText
+    private lateinit var currentFocusedEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,125 +29,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        octalResult = findViewById(R.id.input_octal)
-        hexadecimalResult = findViewById(R.id.input_hex)
-        decimalResult = findViewById(R.id.input_decimal)
-        binaryResult = findViewById(R.id.input_binary)
+        octalEditText = findViewById(R.id.input_octal)
+        hexadecimalEditText = findViewById(R.id.input_hex)
+        decimalEditText = findViewById(R.id.input_decimal)
+        binaryEditText = findViewById(R.id.input_binary)
     }
 
     private fun setupFocusListener() {
-        octalResult.onFocusChangeListener = FocusViewListener()
-        hexadecimalResult.onFocusChangeListener = FocusViewListener()
-        decimalResult.onFocusChangeListener = FocusViewListener()
-        binaryResult.onFocusChangeListener = FocusViewListener()
+        octalEditText.onFocusChangeListener = FocusViewListener()
+        hexadecimalEditText.onFocusChangeListener = FocusViewListener()
+        decimalEditText.onFocusChangeListener = FocusViewListener()
+        binaryEditText.onFocusChangeListener = FocusViewListener()
     }
 
-
-    inner class ViewTextWatcher : TextWatcher {
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        }
-
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            when (currentFocusedView.id) {
-                octalResult.id -> {
-                    if (checkValidInput(currentFocusedView.text.toString(), NumericalBase.OCTAL).not() )
-                        Toast.makeText(this@MainActivity, " enter Invalid Octal number to convert it", Toast.LENGTH_SHORT).show()
-                     convertOctalNumber()
-                }
-                hexadecimalResult.id -> {
-                    if (checkValidInput(currentFocusedView.text.toString(), NumericalBase.HEXADECIMAL).not())
-                        Toast.makeText(this@MainActivity, " enter Invalid Hexadecimal to convert it", Toast.LENGTH_SHORT).show()
-                    convertHexadecimalNumber()
-                }
-                decimalResult.id -> {
-                    if (checkValidInput(currentFocusedView.text.toString(), NumericalBase.DECIMAL).not())
-                        Toast.makeText(this@MainActivity, " enter Invalid decimal number to convert it", Toast.LENGTH_SHORT).show()
-                    convertDecimalNumber()
-                }
-                else -> {
-                    if (checkValidInput(currentFocusedView.text.toString(), NumericalBase.BINARY).not())
-                        Toast.makeText(this@MainActivity, " enter Invalid Binary number ( 0, 1) to convert it", Toast.LENGTH_SHORT).show()
-                    convertBinaryNumber()
-
-                }
+    private fun setOutputForEditText(number: String) {
+        when (currentFocusedEditText.id) {
+            octalEditText.id -> {
+                binaryEditText.setText(baseConverter(NumericalBase.OCTAL, NumericalBase.BINARY, number))
+                hexadecimalEditText.setText(baseConverter(NumericalBase.OCTAL, NumericalBase.HEXADECIMAL, number))
+                decimalEditText.setText(baseConverter(NumericalBase.OCTAL, NumericalBase.DECIMAL, number))
             }
-        }
-
-        override fun afterTextChanged(p0: Editable?) {
-        }
-
-    }
-
-    private fun convertOctalNumber() {
-        binaryResult.setText(toOctal(currentFocusedView.text.toString(), NumericalBase.BINARY))
-        hexadecimalResult.setText(toOctal(currentFocusedView.text.toString(), NumericalBase.HEXADECIMAL))
-        decimalResult.setText(toOctal(currentFocusedView.text.toString(), NumericalBase.DECIMAL))
-    }
-
-    private fun convertHexadecimalNumber() {
-        octalResult.setText(toHexadecimal(currentFocusedView.text.toString(), NumericalBase.OCTAL))
-        binaryResult.setText(toHexadecimal(currentFocusedView.text.toString(), NumericalBase.BINARY))
-        decimalResult.setText(toHexadecimal(currentFocusedView.text.toString(), NumericalBase.DECIMAL))
-    }
-
-    private fun convertDecimalNumber() {
-        octalResult.setText(toDecimal(currentFocusedView.text.toString(), NumericalBase.OCTAL))
-        hexadecimalResult.setText(toDecimal(currentFocusedView.text.toString(), NumericalBase.HEXADECIMAL))
-        binaryResult.setText(toDecimal(currentFocusedView.text.toString(), NumericalBase.BINARY))
-
-    }
-
-    private fun convertBinaryNumber() {
-        octalResult.setText(toBinary(currentFocusedView.text.toString(), NumericalBase.OCTAL))
-        hexadecimalResult.setText(toBinary(currentFocusedView.text.toString(), NumericalBase.HEXADECIMAL))
-        decimalResult.setText(toBinary(currentFocusedView.text.toString(), NumericalBase.DECIMAL))
-    }
-
-
-    inner class FocusViewListener : View.OnFocusChangeListener {
-        override fun onFocusChange(view: View?, hasFocus: Boolean) {
-            if (hasFocus) {
-                currentFocusedView = view as EditText
-                currentFocusedView.addTextChangedListener(textWatcher)
-            } else {
-                currentFocusedView.removeTextChangedListener(textWatcher)
+            hexadecimalEditText.id -> {
+                octalEditText.setText(baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.OCTAL, number))
+                binaryEditText.setText(baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.BINARY, number))
+                decimalEditText.setText(baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.DECIMAL, number))
             }
-        }
-    }
+            decimalEditText.id -> {
+                octalEditText.setText(baseConverter(NumericalBase.DECIMAL, NumericalBase.OCTAL, number))
+                hexadecimalEditText.setText( baseConverter(NumericalBase.DECIMAL, NumericalBase.HEXADECIMAL, number))
+                binaryEditText.setText(baseConverter(NumericalBase.DECIMAL, NumericalBase.BINARY, number))
 
-    private fun toOctal(number: String, base: NumericalBase): String {
-        return when (base) {
-            NumericalBase.BINARY -> baseConverter(NumericalBase.OCTAL, NumericalBase.BINARY, number)
-            NumericalBase.DECIMAL -> baseConverter(NumericalBase.OCTAL, NumericalBase.DECIMAL, number)
-            NumericalBase.HEXADECIMAL -> baseConverter(NumericalBase.OCTAL, NumericalBase.HEXADECIMAL, number)
-            else -> ""
-        }
-    }
+            }
+            binaryEditText.id -> {
+                octalEditText.setText(baseConverter(NumericalBase.BINARY, NumericalBase.OCTAL, number))
+                hexadecimalEditText.setText(baseConverter(NumericalBase.BINARY, NumericalBase.HEXADECIMAL, number))
+                decimalEditText.setText(baseConverter(NumericalBase.BINARY, NumericalBase.DECIMAL, number))
 
-    private fun toHexadecimal(number: String, base: NumericalBase): String {
-        return when (base) {
-            NumericalBase.OCTAL -> baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.OCTAL, number)
-            NumericalBase.DECIMAL -> baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.DECIMAL, number)
-            NumericalBase.BINARY -> baseConverter(NumericalBase.HEXADECIMAL, NumericalBase.BINARY, number)
-            else -> ""
-        }
-    }
-
-    private fun toDecimal(number: String, base: NumericalBase): String {
-        return when (base) {
-            NumericalBase.OCTAL -> baseConverter(NumericalBase.DECIMAL, NumericalBase.OCTAL, number)
-            NumericalBase.HEXADECIMAL -> baseConverter(NumericalBase.DECIMAL, NumericalBase.HEXADECIMAL, number)
-            NumericalBase.BINARY -> baseConverter(NumericalBase.DECIMAL, NumericalBase.BINARY, number)
-            else -> ""
-        }
-    }
-
-    private fun toBinary(number: String, base: NumericalBase): String {
-        return when (base) {
-            NumericalBase.OCTAL -> baseConverter(NumericalBase.BINARY, NumericalBase.OCTAL, number)
-            NumericalBase.DECIMAL -> baseConverter(NumericalBase.BINARY, NumericalBase.DECIMAL, number)
-            NumericalBase.HEXADECIMAL -> baseConverter(NumericalBase.BINARY, NumericalBase.HEXADECIMAL, number)
-            else -> ""
+            }
         }
     }
 
@@ -161,12 +79,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun baseConverter(sourceBase: NumericalBase, targetBase: NumericalBase, numberString: String): String {
+
+        if (checkValidInput(numberString, sourceBase).not() ) return ""
         if (numberString.isBlank()) return ""
 
         val number = numberString.toBigIntegerOrNull(sourceBase.base)
         return if (number == null)  ""
-               else number.toString(targetBase.base)
+        else number.toString(targetBase.base)
 
+    }
+
+    inner class ViewTextWatcher : TextWatcher {
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            setOutputForEditText(text.toString())
+        }
+        override fun afterTextChanged(p0: Editable?) {
+        }
+    }
+
+    inner class FocusViewListener : OnFocusChangeListener {
+        override fun onFocusChange(view: View?, hasFocus: Boolean) {
+            if (hasFocus) {
+                currentFocusedEditText = view as EditText
+                currentFocusedEditText.addTextChangedListener(textWatcher)
+            } else {
+                currentFocusedEditText.removeTextChangedListener(textWatcher)
+            }
+        }
     }
 
 
